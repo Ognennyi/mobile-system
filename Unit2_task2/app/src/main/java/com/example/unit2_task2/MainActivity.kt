3e739a49a09f4e183b54f6ce6494b3ae9fd7df36
Ognennyi <ismailovir5@mail.ru>
2025-10-16 20:19:33 +0300

    unit2_lab1_lambda

---
+++ Unit2_task2/app/src/main/java/com/example/unit2_task2/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.unit2_task2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.unit2_task2.ui.theme.Unit2_task2Theme
+
+fun main() {
+    val treatFunction = trickOrTreat(false) { "$it quarters" }
+    val trickFunction = trickOrTreat(true, null)
+
+    repeat(4) {
+        treatFunction()
+    }
+    trickFunction()
+}
+
+//fun trickOrTreat(isTrick: Boolean): () -> Unit {
+//    if (isTrick){
+//        return trick
+//    }else{
+//        return treat
+//    }
+//
+//}
+
+fun trickOrTreat(
+    isTrick: Boolean,
+    extraTreat: ((Int) -> String)?
+): () -> Unit {
+    if (isTrick) {
+        return trick
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
+
+val trick = {
+    println("No treats!")
+}
+
+val treat = {
+    println("Have a treat!")
+}
