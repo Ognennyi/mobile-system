021c82d391cf6199598663876355cd6eb73e90a0
Ognennyi <ismailovir5@mail.ru>
2025-10-20 12:21:52 +0300

    gotovo

--- unit2_lab1_tasks/app/src/main/java/com/example/unit2_lab1_tasks/MainActivity.kt
+++ unit2_lab1_tasks/app/src/main/java/com/example/unit2_lab1_tasks/MainActivity.kt
@@ -137,6 +137,65 @@ import com.example.unit2_lab1_tasks.ui.theme.Unit2_lab1_tasksTheme
 //}
 
 
+//задача 6 (пункт 7)
+//
+//open class Phone(var isScreenLightOn: Boolean = false){
+//    open fun switchOn() {
+//        isScreenLightOn = true
+//    }
+//
+//    fun switchOff() {
+//        isScreenLightOn = false
+//    }
+//
+//    fun checkPhoneScreenLight() {
+//        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+//        println("The phone screen's light is $phoneScreenLight.")
+//    }
+//}
+//
+//class FoldablePhone(var isFolded: Boolean = true): Phone() {
+//    override fun switchOn() {
+//        if (!isFolded) {
+//            isScreenLightOn = true
+//        }
+//    }
+//
+//    fun fold() {
+//        isFolded = true
+//    }
+//
+//    fun unfold() {
+//        isFolded = false
+//    }
+//}
+//
+//fun main() {
+//    val newFoldablePhone = FoldablePhone()
+//
+//    newFoldablePhone.switchOn()
+//    newFoldablePhone.checkPhoneScreenLight()
+//    newFoldablePhone.unfold()
+//    newFoldablePhone.switchOn()
+//    newFoldablePhone.checkPhoneScreenLight()
+//}
+
+
+
+//задача 7 (пункт 8)
+
+//fun main() {
+//    val winningBid = Bid(5000, "Private Collector")
+//
+//    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+//    println("Item B is sold at ${auctionPrice(null, 3000)}.")
+//}
+//
+//class Bid(val amount: Int, val bidder: String)
+//
+//fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+//    return bid?.amount ?: minimumPrice
+//}
 
 
 
